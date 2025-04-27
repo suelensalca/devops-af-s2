ed10577441f5fcd177d41b8cd3acf870168d9b22
Suelen <suelen.salca@ringa.com.br>
2025-04-27 17:07:44 -0300

    Remove unesed images and change styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 import { Box, Button, Grid, Rating, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import calendar from '../public/assets/illustration-consistent-schedule.webp';
+import audience from '../public/assets/illustration-audience-growth.webp';
+import schedule from '../public/assets/illustration-schedule-posts.webp';
+import followers from '../public/assets/illustration-grow-followers.webp';
 import Image from "next/image";
 
 export default function Home() {
@@ -32,32 +35,38 @@ export default function Home() {
 
 
   return (
-   <Grid container sx={{backgroundColor: '#F1F1FA', height: '740px', padding: '40px'}} spacing={2}>
-    <Grid size={4} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '32px', height: '100%' }}>
-      <Box sx={{backgroundColor: 'seashell', borderRadius: '24px', padding: '32px', height: '70%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Typography sx={{fontSize: '21px', fontWeight: '500', mb: '32px', textAlign: 'left'}}>
-          Crie e agende conteúdo de forma 
-          <span style={{color: '#3D348B'}}> rápida</span>.
-          </Typography>
+   <Grid container sx={{backgroundColor: '#F1F1FA', padding: '20px', minHeight: '100vh'}} spacing={2}>
+    <Grid size={4} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '32px' }}>
+      <Box sx={{backgroundColor: 'seashell', borderRadius: '24px', padding: '32px', height: '60%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <Typography sx={{fontSize: '21px', fontWeight: '500', mb: '20px', textAlign: 'left'}}>
+        Escreva seu conteúdo usando IA.
+        </Typography>
         <Button data-testid="button-post" onClick={changePost} sx={{backgroundColor: '#f7b801', padding: '8px 20px', borderRadius: '40px', color: '#3D348B'}}>crie um Post</Button>
         {showPost && <Typography data-testid="post" sx={{color: '#3D348B', fontSize: '28px', fontWeight: '700', textTransform: 'uppercase', textAlign: 'center', mt: '20px' }}>{posts[currentPost].emoji}{posts[currentPost].text}</Typography>}
       </Box>
-      <Box sx={{backgroundColor: '#f7b801', borderRadius: '24px', padding: '32px', fontSize: '24px', height: '30%' }}>
-        Escreva seu conteúdo usando IA.
+      <Box sx={{backgroundColor: '#f7b801', borderRadius: '24px', padding: '32px', height: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px'}}>
+          <Typography fontSize={24}>
+          Crie e agende conteúdo de forma 
+          <span style={{color: 'white'}}> rápida</span>.
+          </Typography>
+        <Image alt="agenda" src={schedule} width={200} height={160} />
       </Box>
     </Grid>
     <Grid size={4} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '32px' }}>
-      <Box sx={{backgroundColor: 'white', borderRadius: '24px', padding: '32px', height: '40%'}}>
-        <Typography sx={{fontSize: '21px', fontWeight: '500', mb: '32px', color: '#3d348d', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Box sx={{backgroundColor: 'white', borderRadius: '24px', padding: '32px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px'}}>
+        <Typography sx={{fontSize: '21px', fontWeight: '500', mb: '20px', color: '#3d348d', textAlign: 'left'}}>
           Social Media 10x mais rápido com IA.
-          </Typography>
+        </Typography>
         <Rating value={5} />
+        <Image alt="audiencia" src={audience} width={200} height={76} />
       </Box>
-      <Box sx={{backgroundColor: '#BDB9E5', borderRadius: '24px', padding: '32px', fontSize: '24px', height: '30%' }}>
+      <Box sx={{backgroundColor: '#BDB9E5', borderRadius: '24px', padding: '32px', fontSize: '20px' }}>
         Gerencia diferentes contas e plataformas
       </Box>
-      <Box sx={{backgroundColor: '#3D348B', borderRadius: '24px', padding: '32px', fontSize: '24px', height: '30%', color: 'white' }}>
-        Aumente seu crescimento em 54%
+      <Box sx={{backgroundColor: '#3D348B', borderRadius: '24px', padding: '32px', fontSize: '24px', height: '45%', color: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '12px' }}>
+        Aumente seu crescimento
+        <br />
+        <Image alt="seguidores" src={followers} width={230} height={210} />
       </Box>
     </Grid>
     <Grid size={4} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '32px' }}>
